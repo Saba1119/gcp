b38d88fa354d9cd3f99669a8d3e07af301e49c01
Saba1119 <100400271+Saba1119@users.noreply.github.com>
2023-04-20 11:02:26 +0530

    Create instances-firewall.go

---
+++ instances-firewall.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"google.golang.org/api/compute/v1"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// Replace these values with your project ID and zone
+	projectID := "develop-375210"
+	zone := "us-central1-a"
+
+	// Create a new Compute Engine service client
+	computeService, err := compute.NewService(ctx, option.WithScopes(compute.ComputeScope))
+	if err != nil {
+		fmt.Printf("Failed to create Compute Engine service client: %v", err)
+		return
+	}
+
+	// Define the new instance to create
+	instanceName := "my-instance-vm"
+	machineType := fmt.Sprintf("zones/%s/machineTypes/f1-micro", zone)
+	sourceImage := "projects/ubuntu-os-cloud/global/images/family/ubuntu-2204-lts"
+	diskSizeGb := int64(11)
+
+	instance := &compute.Instance{
+		Name:        instanceName,
+		MachineType: machineType,
+		Disks: []*compute.AttachedDisk{
+			{
+				AutoDelete: true,
+				Boot:       true,
+				Mode:       "READ_WRITE",
+				InitializeParams: &compute.AttachedDiskInitializeParams{
+					SourceImage: sourceImage,
+					DiskSizeGb:  diskSizeGb,
+				},
+			},
+		},
+		NetworkInterfaces: []*compute.NetworkInterface{
+			{
+				Network: "global/networks/default",
+				AccessConfigs: []*compute.AccessConfig{
+					{
+						Type:        "ONE_TO_ONE_NAT",
+						Name:        "External NAT",
+						NetworkTier: "PREMIUM",
+					},
+				},
+				Subnetwork: fmt.Sprintf("projects/%s/regions/%s/subnetworks/default", projectID, zone[:len(zone)-2]),
+			},
+		},
+		Tags: &compute.Tags{
+			Items: []string{"http-server", "https-server"},
+		},
+	}
+
+	// Create the new instance
+	op, err := computeService.Instances.Insert(projectID, zone, instance).Do()
+	if err != nil {
+		fmt.Printf("Failed to create instance: %v", err)
+		return
+	}
+
+	fmt.Printf("Instance %q is being created with operation %q\n", instanceName, op.Name)
+
+
+	// Wait for the instance to be created
+	fmt.Printf("Waiting for the instance to be created...\n")
+	for {
+		op, err := computeService.ZoneOperations.Get(projectID, zone, op.Name).Do()
+		if err != nil {
+			fmt.Printf("Failed to get zone operation %q: %v", op.Name, err)
+			return
+		}
+
+		if op.Status == "DONE" {
+			if op.Error != nil {
+				fmt.Printf("Failed to create instance: %v", op.Error.Errors)
+				return
+			}
+
+			fmt.Printf("Instance %q is created.\n", instanceName)
+			break
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+
+	// Define the new firewall rule to allow ingress and egress traffic on port 80 and 443
+	firewallName := "my-firewal-rule"
+	firewall := &compute.Firewall{
+		Name: firewallName,
+		Allowed: []*compute.FirewallAllowed{
+			{
+				IPProtocol: "tcp",
+				Ports:      []string{"8000", "8080"},
+			},
+		},
+		Network: fmt.Sprintf("projects/%s/global/networks/default", projectID),
+		SourceRanges: []string{
+			"0.0.0.0/0",
+		},
+		DestinationRanges: []string{
+			"0.0.0.0/0",
+		},
+		TargetTags: []string{"http-server", "https-server"},
+	}
+
+	// Create the new firewall rule
+	_, err = computeService.Firewalls.Insert(projectID, firewall).Do()
+	if err != nil {
+		fmt.Printf("Failed to create firewall rule: %v", err)
+		return
+	}
+
+	fmt.Printf("Firewall rule %q is created.\n", firewallName)
+
+	// Get the new instance to update its network tags
+	newInstance, err := computeService.Instances.Get(projectID, zone, instanceName).Do()
+	if err != nil {
+		fmt.Printf("Failed to get instance: %v", err)
+		return
+	}
+
+	// Add the network tags to the new instance
+	newInstance.Tags = &compute.Tags{
+		Items: []string{
+			"web-server",
+		},
+	}
+
+	// Update the new instance to add the network tags
+	_, err = computeService.Instances.Update(projectID, zone, instanceName, newInstance).Do()
+	if err != nil {
+		fmt.Printf("Failed to update instance: %v", err)
+		return
+	}
+
+	fmt.Printf("Instance %q is updated with network tags.\n", instanceName)
+}
